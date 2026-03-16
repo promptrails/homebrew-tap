424f6543e67916ab065042faeb9b811a30510321
Bahattin Cinic <bahattin@promptrails.ai>
2026-03-16 18:48:27 +0300

    feat: added v0.1

--- Casks/promptrails-monitor.rb
+++ Casks/promptrails-monitor.rb
@@ -3,12 +3,12 @@ cask "promptrails-monitor" do
 
   on_intel do
     url "https://github.com/promptrails/desktop/releases/download/v#{version}/PromptRails.Monitor_#{version}_x64.dmg"
-    sha256 :no_check
+    sha256 "ddb12879856bceba19490150e410cc971df941dc2f3c74acadef5bcbdd5766d0"
   end
 
   on_arm do
     url "https://github.com/promptrails/desktop/releases/download/v#{version}/PromptRails.Monitor_#{version}_aarch64.dmg"
-    sha256 :no_check
+    sha256 "4deff4128fd8db121c873386ea2f81aa41d07bac53f763d9151d6eaad7259e18"
   end
 
   name "PromptRails Monitor"
