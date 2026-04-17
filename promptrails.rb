d69f3f3f9f3463a7d90592533badbc0bb46607c0
Bahattin Cinic <bahattin@promptrails.ai>
2026-04-17 20:38:08 +0300

    bump promptrails to v0.3.0

--- promptrails.rb
+++ promptrails.rb
@@ -1,30 +1,30 @@
 class Promptrails < Formula
   desc "CLI for the PromptRails AI agent orchestration platform"
   homepage "https://github.com/promptrails/cli"
-  version "0.2.1"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/promptrails/cli/releases/download/v0.2.1/promptrails-darwin-amd64.tar.gz"
-      sha256 "6b253f576aa3245c3bca02b155a823adac78c269ad7b452e8ef8b7687cb08651"
+      url "https://github.com/promptrails/cli/releases/download/v0.3.0/promptrails-darwin-amd64.tar.gz"
+      sha256 "7cf4f6d6d9f43df0df1d318d0120d6b00d5f840004361d4dd0f79b2f0687aee2"
     end
 
     on_arm do
-      url "https://github.com/promptrails/cli/releases/download/v0.2.1/promptrails-darwin-arm64.tar.gz"
-      sha256 "3c3df32a3644d67a5e77a996dbafad4d473a3870ce7761ba5205ae45c7de7c7a"
+      url "https://github.com/promptrails/cli/releases/download/v0.3.0/promptrails-darwin-arm64.tar.gz"
+      sha256 "e066114d26524498974f1afcbde56f92c699d8f2cbb468608559deab4c39ef5b"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/promptrails/cli/releases/download/v0.2.1/promptrails-linux-amd64.tar.gz"
-      sha256 "50f1b93e95631eff736d4acb9e40ee4ea74e28a3dd09188ed1706c5bfbc595f2"
+      url "https://github.com/promptrails/cli/releases/download/v0.3.0/promptrails-linux-amd64.tar.gz"
+      sha256 "9eed9294c9cc57f2b79d0be3b7b0334c9a9b3f64597be4078df3bb6559d560d2"
     end
 
     on_arm do
-      url "https://github.com/promptrails/cli/releases/download/v0.2.1/promptrails-linux-arm64.tar.gz"
-      sha256 "8fd797275a90aa0e5f2ccb62b6bf22d130f526569605c147dd0aa8628fda2552"
+      url "https://github.com/promptrails/cli/releases/download/v0.3.0/promptrails-linux-arm64.tar.gz"
+      sha256 "7188303b8b5c16ad0bda0f47669c78eea61f1c289a30669f5043f3553fd05f8e"
     end
   end
 
